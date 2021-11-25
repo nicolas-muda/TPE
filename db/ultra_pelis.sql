91ad0ba53be85a1d14dd6c629d5f6c503e9e709f
nicolas muda <nicolasmuda@live.com.ar>
2021-11-24 21:50:32 -0300

    se remplazo la base de datos por la ultima version

--- db/ultra_pelis.sql
+++ db/ultra_pelis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-10-2021 a las 03:09:21
+-- Tiempo de generación: 25-11-2021 a las 01:49:24
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.6
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ultra_pelis`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `comentario` varchar(300) NOT NULL,
+  `puntuacion` int(5) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fecha_comentario` date NOT NULL,
+  `id_pelicula` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `comentario`, `puntuacion`, `id_usuario`, `fecha_comentario`, `id_pelicula`) VALUES
+(22, 'muy buena pelicula', 5, 1, '2021-11-23', 1),
+(23, 'no es lo que me espera pero esta buena', 3, 15, '2021-11-09', 1),
+(24, 'muy buena continuación de la saga', 5, 16, '2021-11-03', 1),
+(25, 'no me gusto para nada muy mala', 1, 4, '2021-11-24', 1),
+(29, 'me esperaba mucho mas de la tercera parte de la saga', 2, 3, '2021-11-24', 1);
 
 -- --------------------------------------------------------
 
@@ -41,8 +67,7 @@ INSERT INTO `generos` (`id_generos`, `genero`) VALUES
 (2, 'comedia'),
 (3, 'accion'),
 (4, 'ciencia ficcion'),
-(5, 'drama'),
-(7, 'fantasia');
+(5, 'drama');
 
 -- --------------------------------------------------------
 
@@ -72,7 +97,7 @@ INSERT INTO `peliculas` (`id_pelicula`, `duracion`, `descripcion`, `id_genero_fk
 (20, '1h 41m', 'John Wick es un asesino a sueldo retirado que vuelve a la acción después de que un jefe mafioso haya puesto precio a su cabeza y le haya robado todo lo que le quedaba tras la muerte de su esposa.', 3, 'john wick', 10),
 (21, '1h 50m', 'Cole Young, el luchador de MMA, acostumbrado a recibir palizas por dinero, desconoce su ascendencia, y tampoco sabe por qué el emperador Shang Tsung de Outworld ha enviado a su mejor guerrero, Sub-Zero, un Cryomancer sobrenatural, para dar caza a Cole.', 3, 'mortal kombat', 7),
 (25, '1h 37m', 'Después de encontrar un cuerpo anfitrión en el periodista de investigación Eddie Brock, el simbionte alienígena debe enfrentarse a un nuevo enemigo, Carnage, el alter ego del asesino en serie Cletus Kasady.', 3, 'Venom: Carnage liberado', 8),
-(26, '2h 1m', 'Adelaide Wilson, su marido y sus dos hijos visitan la casa en la que ella creció junto a la playa. Allí, Adelaide tiene un presentimiento siniestro que precede a un encuentro espeluznante: cuatro enmascarados se presentan ante su casa.', 1, 'us', 6);
+(26, '2h 1m', 'Adelaide Wilson, su marido y sus dos hijos visitan la casa en la que ella creció junto a la playa. Allí, Adelaide tiene un presentimiento siniestro que precede a un encuentro espeluznante: cuatro enmascarados se presentan ante su casa', 1, 'us', 7);
 
 -- --------------------------------------------------------
 
@@ -83,21 +108,32 @@ INSERT INTO `peliculas` (`id_pelicula`, `duracion`, `descripcion`, `id_genero_fk
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `contraseña` varchar(250) NOT NULL
+  `contraseña` varchar(250) NOT NULL,
+  `rol` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `email`, `contraseña`) VALUES
-(1, 'nicolasmuda@live.com.ar', '$2a$12$xu0KBwyv.wisEtsx/uiZE.F2fJml0AMTfH3vXyCNfrlgeTszzKyee'),
-(3, 'prueba@live.com.ar', '$2a$12$gEd35lEz.rkAvufgwt/XL.mtgB1iR.5yK6ZcDFd.1bng/W94E6ugm'),
-(4, 'testeo@gmail.com', '$2a$12$i/KijIrmmyV6YHR65T4Ew.cY3/eVYXvG5.DgV8XXQnG5c.xJhgtxC');
+INSERT INTO `usuarios` (`id`, `email`, `contraseña`, `rol`) VALUES
+(1, 'nicolasmuda@live.com.ar', '$2a$12$xu0KBwyv.wisEtsx/uiZE.F2fJml0AMTfH3vXyCNfrlgeTszzKyee', 'administrador'),
+(3, 'prueba@live.com.ar', '$2a$12$gEd35lEz.rkAvufgwt/XL.mtgB1iR.5yK6ZcDFd.1bng/W94E6ugm', 'usuario'),
+(4, 'testeo@gmail.com', '$2a$12$i/KijIrmmyV6YHR65T4Ew.cY3/eVYXvG5.DgV8XXQnG5c.xJhgtxC', 'administrador'),
+(15, 'usuario1@gmail.com', '$2y$10$eliXWqWFYbCxRZuCH4ZZiOqBZ6sE01LLfwOhKs2lbhOEW./.1jYRu', 'usuario'),
+(16, 'usuario2@live.com', '$2y$10$fd.GSJdRmEu6BOMCXneRj.8YGTnV76gvMcMgWFyIjA21J06K997k6', 'usuario');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pelicula` (`id_pelicula`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `generos`
@@ -123,32 +159,45 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
 -- AUTO_INCREMENT de la tabla `generos`
 --
 ALTER TABLE `generos`
-  MODIFY `id_generos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_generos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  MODIFY `id_pelicula` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id_pelicula` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_pelicula`) REFERENCES `peliculas` (`id_pelicula`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+--
 -- Filtros para la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  ADD CONSTRAINT `peliculas_ibfk_1` FOREIGN KEY (`id_genero_fk`) REFERENCES `generos` (`id_generos`),
+  ADD CONSTRAINT `peliculas_ibfk_1` FOREIGN KEY (`id_genero_FK`) REFERENCES `generos` (`id_generos`),
   ADD CONSTRAINT `peliculas_ibfk_2` FOREIGN KEY (`id_genero_fk`) REFERENCES `generos` (`id_generos`);
 COMMIT;
 
